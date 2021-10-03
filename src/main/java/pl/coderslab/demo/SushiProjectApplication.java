bafcae8b011d8dc07afb8575150b9289b6bbb16c
TazbeeR <myszkiewicz.robert81@gmail.com>
2021-10-03 17:21:46 +0200

    pakiet konfiguracyjny

---
+++ src/main/java/pl/coderslab/demo/SushiProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiProjectApplication.class, args);
+	}
+
+}
